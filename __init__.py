b3e0103b68523ab2c8e6eba7acddd1fd5a516f57
micah-0w0 <tinyviolin1@gmail.com>
2024-07-09 21:59:28 -0400

    Update __init__.py
    I think I finished step 4

--- __init__.py
+++ __init__.py
@@ -1,22 +1,23 @@
-import sys
-from binaryninja.log import log_info
+=from binaryninja.log import log_info
 from binaryninja.binaryview import BinaryViewType
 from binaryninja.plugin import PluginCommand
 from binaryninja.plugin import BackgroundTaskThread
-from binaryninja.enums import (MediumLevelILOperation, RegisterValueType)
+from binaryninja.enums import MediumLevelILOperation
+
 
 class MBADeobfuscationInBackground(BackgroundTaskThread):
-  def __init__(self, bv, msg):
-    BackgroundTaskThread.__init__(self, msg, True)
-    self.bv = bv
+    def __init__(self, bv, msg):
+        BackgroundTaskThread.__init__(self, msg, True)
+        self.bv = bv
 
-  def run(self):
-    for instr in self.bv.mlil_instructions:
-	    if instr.operation == MediumLevelILOperation.MLIL_IF:
-		    log_info(instr)
+    def run(self):
+	    for instr in self.bv.mlil_instructions:
+            if instr.operation == MediumLevelILOperation.MLIL_IF:
+                log_info(instr)
 
 
-def mba_deobfuscation_in_background(self):
-  # step 3 - how?
-  # instance = 
-  print("what now")
+def mba_deobfuscation_in_background(bv):
+    background_task = MBADeobfuscationInBackground(bv, "Starting MBA Deobfuscation")
+    background_task.start()
+
+PluginCommand.register("MBA Deobfuscation", "Simplifying booleans" , mba_deobfuscation_in_background)
