7d0642ec9ed8c3eb08d1070a003332961601fdd2
micah-0w0 <tinyviolin1@gmail.com>
2024-07-09 20:54:48 -0400

    Update __init__.py
    I think I did the step 2

--- __init__.py
+++ __init__.py
@@ -1,6 +1,22 @@
+import sys
+from binaryninja.log import log_info
+from binaryninja.binaryview import BinaryViewType
+from binaryninja.plugin import PluginCommand
+from binaryninja.plugin import BackgroundTaskThread
+from binaryninja.enums import (MediumLevelILOperation, RegisterValueType)
+
 class MBADeobfuscationInBackground(BackgroundTaskThread):
-  def __init__(self):
-    print("what do i do in this constructor")
+  def __init__(self, bv, msg):
+    BackgroundTaskThread.__init__(self, msg, True)
+    self.bv = bv
 
   def run(self):
-    
+    for instr in self.bv.mlil_instructions:
+	    if instr.operation == MediumLevelILOperation.MLIL_IF:
+		    log_info(instr)
+
+
+def mba_deobfuscation_in_background(self):
+  # step 3 - how?
+  # instance = 
+  print(
