4759412fcb6ce76a566c918b1dd351877a4c6711
micah-0w0 <tinyviolin1@gmail.com>
2024-07-11 13:26:05 -0400

    Updated __init__.py requested changes

--- __init__.py
+++ __init__.py
@@ -6,23 +6,25 @@ from binaryninja.enums import MediumLevelILOperation
 
 
 class MBADeobfuscationInBackground(BackgroundTaskThread):
-    '''Assigns a thread to MBA deobfuscation'''
-    
-    def __init__(self, bv : BinaryViewType, msg : str):
-        '''Initiates the MBADeobfuscationInBackground object and defines bv attribute'''
+    """Assigns a thread to MBA deobfuscation"""
+
+    def __init__(self, bv: BinaryViewType, msg: str):
+        """Initiates the MBADeobfuscationInBackground object and defines bv attribute"""
         BackgroundTaskThread.__init__(self, msg, True)
         self.bv = bv
 
     def run(self):
-        '''Logs all program if statements to BinaryNinja log'''
+        """Logs all program if statements to BinaryNinja log"""
         for instr in self.bv.mlil_instructions:
             if instr.operation == MediumLevelILOperation.MLIL_IF:
                 log_info(instr)
 
 
-def mba_deobfuscation_in_background(bv : BinaryViewType):
-    '''Creates a background task and starts MBA deobfuscation'''
-    background_task : MBADeobfuscationInBackground = MBADeobfuscationInBackground(bv, "Starting MBA Deobfuscation")
+def mba_deobfuscation_in_background(bv: BinaryViewType):
+    """Creates a background task and starts MBA deobfuscation"""
+    background_task: MBADeobfuscationInBackground = MBADeobfuscationInBackground(
+        bv, "Starting MBA Deobfuscation"
+    )
     background_task.start()
 
 
