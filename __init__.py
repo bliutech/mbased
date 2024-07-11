e6a2e78b6fe492f3ba5b2d4fdf37b7dbf25ba4d5
micah-0w0 <tinyviolin1@gmail.com>
2024-07-11 13:17:32 -0400

    Update __init__.py
    Added requested issue changes

--- __init__.py
+++ __init__.py
@@ -6,18 +6,23 @@ from binaryninja.enums import MediumLevelILOperation
 
 
 class MBADeobfuscationInBackground(BackgroundTaskThread):
-    def __init__(self, bv, msg):
+    '''Assigns a thread to MBA deobfuscation'''
+    
+    def __init__(self, bv : BinaryViewType, msg : str):
+        '''Initiates the MBADeobfuscationInBackground object and defines bv attribute'''
         BackgroundTaskThread.__init__(self, msg, True)
         self.bv = bv
 
     def run(self):
+        '''Logs all program if statements to BinaryNinja log'''
         for instr in self.bv.mlil_instructions:
             if instr.operation == MediumLevelILOperation.MLIL_IF:
                 log_info(instr)
 
 
-def mba_deobfuscation_in_background(bv):
-    background_task = MBADeobfuscationInBackground(bv, "Starting MBA Deobfuscation")
+def mba_deobfuscation_in_background(bv : BinaryViewType):
+    '''Creates a background task and starts MBA deobfuscation'''
+    background_task : MBADeobfuscationInBackground = MBADeobfuscationInBackground(bv, "Starting MBA Deobfuscation")
     background_task.start()
 
 
