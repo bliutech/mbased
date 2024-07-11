36419135a2e0dcc7e0205e7cc957d9a71119eb49
micah-0w0 <tinyviolin1@gmail.com>
2024-07-11 15:16:37 -0400

    Update parse.py
    Wrote a basic parser class that needs functionality

---
+++ parser/parse.py
@@ -0,0 +1,16 @@
+class Parser:
+    '''Parser object for syntax analyses'''
+    
+    def __init__(self):
+        '''Initializes the parser object'''
+        print("Initializing the parser")
+
+    def parse(self, tokens : list[str]):
+        '''Takes in the tokens and parses them with the LL(1) grammar'''
+        print("Parsing...")
+        print("Here are the tokens: ")
+        print(tokens)
+
+        ast = "I am an AST"
+        assert ast == "I am an AST", "There was a parsing error."
+        return ast
