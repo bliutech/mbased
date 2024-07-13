8fa2d5650d3fa39069ed0f253fabaf8b155d15ae
Nytro1O1 <35637473+Nytro1O1@users.noreply.github.com>
2024-07-13 11:11:54 -0400

    Update test_coding.py

--- tests/test_coding.py
+++ tests/test_coding.py
@@ -1,4 +1,4 @@
-test_cases : dict[str] = {
+test_cases : dict[str, str] = {
     "if (([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || ![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b" : "( ( A | ! A & B ) | ! C )"
     "if ([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b" : "( A | ! A )"
     "while (x == 3 && y >= 2):" : "A & B"
