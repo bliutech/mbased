e5ba2f2c6b1b566d36f37c8d409641c40fd00f82
sanjanamandadi <sanjanamandadi@gmail.com>
2024-07-13 23:40:45 -0400

    fix: Update lex.py
    Ok I switched back to the right branch. Not sure if that creates any other problems though. I'll put the unit tests in the test folder

---
+++ parser/lex.py
@@ -0,0 +1,37 @@
+import re
+import sys
+
+class Lexer:
+    terminals: str = r"\&|\||\!|\(|\)|[A-Z]+"
+    ws: str = r"\s|\t|\n|\r"
+    eof: str = r"\Z"
+
+    def __init__(self):
+        self.tokens: list[str] = []
+
+    def error(msg: str):
+        print(f"Lex error: {msg}", file=sys.stderr)
+        exit(1)
+
+    def getTokens(self) -> list[str]:
+        return self.tokens
+
+    @staticmethod
+    def lex(self, prog: str):
+        p: re.Pattern = re.compile(f"{Lexer.terminals}|{Lexer.ws}|{Lexer.eof}")
+        last: int = 0
+        for m in p.finditer(prog):
+            token: str = m.group()
+
+            if m.start() != last:
+                Lexer.error(
+                    f"Invalid character, {prog[last]}, at {m.start()}:{m.end()}"
+                )
+
+            if not re.match(Lexer.ws, token):
+                if re.match(Lexer.eof, token):
+                    self.tokens.append("<EOF>")
+                else:
+                    self.tokens.append(token)
+
+            last = m.end()
