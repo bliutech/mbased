fe945c40189b0e96310a6d4876f77fa91592badb
sanjanamandadi <sanjanamandadi@gmail.com>
2024-07-14 19:34:00 -0400

    feat: unit tests for lexer

---
+++ tests/test_lex.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestLexer(unittest.TestCase):
+
+    def test_lex(self):
+        prog: str = "(A & B) | !C"
+        tokens: list[str] = Lexer.lex(prog)
+        self.assertEqual(tokens, ["(", "A", "&", "B", ")", "|", "!", "C"])
+
+        prog = "A|B"
+        tokens = Lexer.lex(prog)
+        self.assertEqual(tokens, ["A", "|", "B"])
+
+        prog = "!(A & C)"
+        tokens = Lexer.lex(prog)
+        self.assertEqual(tokens, ["!", "(", "A", "&", "C", ")"])
