f692a515ecf007c07dce053fe4943c48dc4c1fac
timoslater <tsla0307@gmail.com>
2024-07-13 22:17:55 -0400

    docs: add docstrings to AST node classes

--- parser/ast.py
+++ parser/ast.py
@@ -2,14 +2,35 @@ from typing import Union
 
 
 class Term:
-    def __init__(self, value: str) -> None:
-        self.value = value
+    """
+    Represents a single term in a binary expression
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        the encoded name of the term
+    """
+
+    def __init__(self, name: str) -> None:
+        """
+        Parameters
+        ----------
+        name : str
+            the encoded name of the term
+        """
+        self.value = name
 
     def __str__(self) -> str:
         return self.value
 
 
 class And:
+    """
+    Represents the AND (&) operator in a boolean expression
+    """
+
     def __init__(self) -> None:
         pass
 
@@ -18,6 +39,10 @@ class And:
 
 
 class Or:
+    """
+    Represents the OR (|) operator in a boolean expression
+    """
+
     def __init__(self) -> None:
         pass
 
@@ -26,6 +51,10 @@ class Or:
 
 
 class Not:
+    """
+    Represents the NOT (!) operator in a boolean expression
+    """
+
     def __init__(self) -> None:
         pass
 
@@ -34,12 +63,41 @@ class Not:
 
 
 class Expr:
+    """
+    Represents a full binary expression
+
+    ...
+
+    Attributes
+    ----------
+    left : Expr/Term
+        The operand left of the expression's operator. Can be a Term or another Expr.
+
+    right : Expr/Term
+        The operand right of the expression's operator. Can be a Term or another Expr.
+
+    operator : And/Or/Not
+        The operator of the boolean expression.
+    """
+
     def __init__(
         self,
         left: Union["Expr", "Term"],
         right: Union["Expr", "Term"],
         operator: Union["And", "Or", "Not"],
     ) -> None:
+        """
+        Parameters
+        ----------
+        left : Expr/Term
+            The operand left of the expression's operator. Can be a Term or another Expr.
+
+        right : Expr/Term
+            The operand right of the expression's operator. Can be a Term or another Expr.
+
+        operator : And/Or/Not
+            The operator of the boolean expression.
+        """
 
         self.left = left  # left operand
         self.right = right  # right operand
