208f67c4b53800e5eee3f619b433fed4e7d37b8b
Nytro1O1 <35637473+Nytro1O1@users.noreply.github.com>
2024-07-15 15:59:44 -0400

    Update test_coding.py - Added Test classes

--- tests/test_coding.py
+++ tests/test_coding.py
@@ -1,3 +1,16 @@
+class TestNameGenerator(unittest.TestCase):
+    def test_generate_name(self) -> None:
+        name_gen: NameGenerator = NameGenerator()
+
+
+class TestDictionaryEncoder(unittest.TestCase):
+    def test_encode(self) -> None:
+        encoder: DictionaryEncoder = DictionaryEncoder()
+        encoded_str: str = encoder.encode(
+            "if (([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || (![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b"
+        )
+
+
 test_cases : dict[str, str] = {
     "if (([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || ![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b" : "( ( A | ! A & B ) | ! C )"
     "if ([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b" : "( A | ! A )"
