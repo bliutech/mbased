81cd67da41d2e4fc5aa0340d8587ad46094eccba
sanjanamandadi <sanjanamandadi@gmail.com>
2024-07-16 16:08:18 -0400

    feat: Update visitor.py

---
+++ parser/visitor.py
@@ -0,0 +1,22 @@
+from parser import ast
+
+
+class Visitor:
+
+    def traverseAST():
+        
+
+    def visitOperator():
+        pass
+
+    def visitVar():
+        pass
+
+    def visitExpr():
+        pass
+
+    def visitExprPrime():
+        pass
+
+    def visitParenExpr():
+        pass
