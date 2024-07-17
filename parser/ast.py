e4202c2ea246cd7d011ef2f461b4edfdaf1bd3f8
timoslater <tsla0307@gmail.com>
2024-07-16 22:25:58 -0400

    refactor: add VarExpr, convert Expr and ExprPrime to parent classes/interfaces

--- parser/ast.py
+++ parser/ast.py
@@ -3,9 +3,16 @@ from enum import Enum
 
 
 class Var:
+    """
+    A class to represent a variable from A-Z.
+
+    Attributes
+    ----------
+    name : str
+        The name of the variable.
+    """
 
     def __init__(self, name: str):
-
         self.name = name
 
     def __str__(self) -> str:
@@ -13,33 +20,39 @@ class Var:
 
 
 class Expr:
+    """
+    An interface class to represent a full expression.
+    """
 
-    def __init__(self, first: Var | Expr, second: ExprPrime = None):
-
-        self.first = first
-        self.second = second
-
-    def __str__(self) -> str:
-
-        if self.second is None:
-            return str(self.first)  # when ExprPrime is not needed or nullable
-
-        return f"{self.first} {self.second}"  # when Expr = Var ExprPrime and ExprPrime is not nullable
+    pass
 
 
 class ExprPrime:
+    """
+    An interface class to represent a prime expression. Used to eliminate left-recursion.
+    """
 
-    def __init__(self, first: ExprPrime):
-        self.first = first
+    pass
 
-    def __str__(self) -> str:
-        return str(self.first)
+    # def __init__(self, first: ExprPrime = None):
+    #  self.first = first
+    # def __str__(self) -> str:
+    #  if self.first is None:
+    #      return "ε"
+    #  return str(self.first)
 
 
 class AndExpr(ExprPrime):
+    """
+    A class to represent an AND expression.
+
+    Attributes
+    ----------
+    first : Expr
+        The first part of the AND expression.
+    """
 
     def __init__(self, first: Expr):
-
         self.first = first
 
     def __str__(self) -> str:
@@ -47,6 +60,14 @@ class AndExpr(ExprPrime):
 
 
 class OrExpr(ExprPrime):
+    """
+    A class to represent an OR expression.
+
+    Attributes
+    ----------
+    first : Expr
+        The first part of the OR expression.
+    """
 
     def __init__(self, first: Expr):
         self.first = first
@@ -55,7 +76,39 @@ class OrExpr(ExprPrime):
         return f"| {self.first}"
 
 
+class VarExpr(Expr):
+    """
+    A class to represent an expression containing only a Var node.
+
+    Attributes
+    ----------
+    first : Var
+        The variable contained by the VarExpr.
+
+    second : ExprPrime, Optional
+        Optional attribute for containing the "AndExpr", "OrExpr", or "None" (for epsilon case).
+    """
+
+    def __init__(self, first: Var, second: ExprPrime = None):
+        self.first = first
+        self.second = second
+
+    def __str__(self) -> str:
+        if not self.second:
+            return str(self.first)
+
+        return f"{self.first} {self.second}"
+
+
 class NotExpr(Expr):
+    """
+    A class to represent a NOT expression.
+
+    Attributes
+    ----------
+    first : Expr
+        The first part of the NOT expression.
+    """
 
     def __init__(self, first: Expr):
         self.first = first
@@ -65,6 +118,15 @@ class NotExpr(Expr):
 
 
 class ParenExpr(Expr):
+    """
+    A class to represent a parenthesized expression.
+
+    Attributes
+    ----------
+    first : Expr
+        The first part of the parenthesized expression.
+    """
+
     def __init__(self, first: Expr):
         self.first = first
 
