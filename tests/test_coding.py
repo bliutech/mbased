17ef9357a03b7968c7af7b80fd5ce06e91a24da3
Nytro1O1 <35637473+Nytro1O1@users.noreply.github.com>
2024-07-17 21:54:18 -0400

    Update test_coding.py - added TestDictionaryEncoderDecoder class

--- tests/test_coding.py
+++ tests/test_coding.py
@@ -2,31 +2,50 @@ from unittest import TestCase
 from utils.coding import DictionaryEncoder
 
 
-class TestDictionaryEncoder(TestCase):
+class TestDictionaryEncoderDecoder(unittest.TestCase):
     """
-    A class to test the DictionaryEncoder class.
+    A class to test the DictionaryEncoder and DictionaryDecoder classes.
 
     Methods
     -------
-    test_encode() -> None:
-        Tests the encode method.
+    test_encode_decode() -> None:
+        Tests the encode and decode method.
     """
 
-    test_cases: dict[str, str] = {
-        "if (([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || ![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b": "( ( A | ! A & B ) | ! C )",
-        "if ([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b": "( A | ! A )",
-        "while (x == 3 && y >= 2):": "( A & B )",
-        "if ([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || (![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b": "( A | ! A & B ) | ( ! C )",
-    }
-
-    def test_encode(self) -> None:
+    def test_encode_decode(self) -> None:
         """
         Tests the encode method.
         """
-        for expected, actual in self.test_cases.items():
-            with self.subTest(expected=expected, actual=actual):
-                encoder: DictionaryEncoder = DictionaryEncoder()
-                encoded_str: str = encoder.encode(expected)
-                self.assertEqual(
-                    encoded_str, actual, "Two values are not equal to each other..."
-                )
+        test_cases_encoded: dict[str, str] = {
+            "if (([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || ![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b": "( ( A | ! A & ! B ) | ! C )",
+            "if ([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b": "( A | ! A )",
+            "while (x == 3 && y >= 2):": "( A & B )",
+            "if ([ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0) || (![ebp_1 + 0x14] == 0) then 387 @ 0x8040da8 else 388 @ 0x8040d8b": "( A | ! A & ! B ) | ( ! C )",
+        }
+
+        test_cases_decoded: dict[str, str] = {
+            "( ( A | ! A & ! B ) | ! C )": "( ( [ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0 ) || [ebp_1 + 0x14] != 0 )",
+            "( A | ! A )": "( [ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 )",
+            "( A & B )": "( x == 3 && y >= 2 )",
+            "( A | ! A & ! B ) | ( ! C )": "( [ebp_1 + 0x14].b == 0 || [ebp_1 + 0x14].b != 0 && [ebp_1 + 0x14].c != 0 ) || ( [ebp_1 + 0x14] != 0 )",
+        }
+
+        for encoded_test in test_cases_encoded:
+            encoder: DictionaryEncoder = DictionaryEncoder()
+            encoded_str: str = encoder.encode(encoded_test)
+            answer: str = test_cases_encoded.get(encoded_test)
+            self.assertEqual(
+                encoded_str, answer, "Two values are not equal to each other..."
+            )
+            # print(encoder.get_encoded_dictionary())
+
+            decoder: DictionaryDecoder = DictionaryDecoder(
+                encoder.get_encoded_dictionary()
+            )
+            decoded_str: str = decoder.decode(answer)
+            # print(decoded_str)
+            answer: str = test_cases_decoded.get(answer)
+            # print(answer)
+            self.assertEqual(
+                decoded_str, answer, "Two values are not equal to each other..."
+            )
