2f5543212151e7bcbd80a0a123a90ed93df0c82b
micah-0w0 <tinyviolin1@gmail.com>
2024-07-18 18:51:48 -0400

    fix: simplified code and added doc string

--- utils/metrics.py
+++ utils/metrics.py
@@ -5,43 +5,24 @@ from parser.visitor import Visitor
 
 
 class OpCounter(Visitor):
+    """Counts the number of boolean operators visited"""
 
-    _count = 0
-
-    def __init__(self):
-        self._count: int = 0
-
-    @override
-    def visitVarExpr(self, vex: VarExpr) -> None:
-        vex.first.accept(self)
-        if vex.second:
-            vex.second.accept(self)
+    _count: int = 0
 
     @override
     def visitNotExpr(self, nex: NotExpr) -> None:
-        self.updateCount()
+        OpCounter._count += 1
         nex.first.accept(self)
 
     @override
-    def visitParenExpr(self, pex: ParenExpr) -> None:
-        pex.first.accept(self)
-
-    @override
     def visitAndExpr(self, aex: AndExpr) -> None:
-        self.updateCount()
+        OpCounter._count += 1
         aex.first.accept(self)
 
     @override
     def visitOrExpr(self, oex: OrExpr) -> None:
-        self.updateCount()
+        OpCounter._count += 1
         oex.first.accept(self)
-
-    @override
-    def visitVar(self, _) -> None:
-        pass
-
-    def updateCount(self) -> None:
-        self._count += 1
 
     def getCount(self) -> None:
         return self._count
