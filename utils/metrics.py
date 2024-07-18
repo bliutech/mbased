19d5db03c58b0d5d1dba610eff7014e6b68e0c91
micah-0w0 <tinyviolin1@gmail.com>
2024-07-18 15:16:38 -0400

    Add files via upload

---
+++ utils/metrics.py
@@ -0,0 +1,37 @@
+from typing import override
+
+from parser.ast import VarExpr, NotExpr, ParenExpr, AndExpr, OrExpr
+from parser.visitor import Visitor
+
+
+class OpCounter(Visitor):
+
+    def __init__(self):
+        self._count = 0
+
+    @override
+    def visitVarExpr(self, vex: VarExpr) -> None:
+        OpCounter._count += 1
+
+    @override
+    def visitNotExpr(self, nex: NotExpr) -> None:
+        OpCounter._count += 1
+
+    @override
+    def visitParenExpr(self, pex: ParenExpr) -> None:
+        OpCounter._count += 1
+
+    @override
+    def visitAndExpr(self, aex: AndExpr) -> None:
+        OpCounter._count += 1
+
+    @override
+    def visitOrExpr(self, oex: OrExpr) -> None:
+        OpCounter._count += 1
+
+    @override
+    def visitVar(self, _) -> None:
+        OpCounter._count += 1
+
+    def get_count(self):
+        return self._count
